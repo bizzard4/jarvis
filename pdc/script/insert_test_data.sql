b5700282334311e685bccc4377dfa6de189d2ad3
François Gingras <francois.gingras89@gmail.com>
2016-11-28 01:21:34 -0500

    Fixed a bug where sequence number were not updated when inserting test data

--- pdc/script/insert_test_data.sql
+++ pdc/script/insert_test_data.sql
@@ -1,3 +1,7 @@
 COPY subjects FROM 'D:\Programmation\GIT\jarvis\pdc\script\test_data\subjects_test_data.csv' DELIMITER ',' CSV;
 COPY value_types FROM 'D:\Programmation\GIT\jarvis\pdc\script\test_data\value_types_test_data.csv' DELIMITER ',' CSV;
 COPY entries FROM 'D:\Programmation\GIT\jarvis\pdc\script\test_data\entries_test_data.csv' DELIMITER ',' CSV;
+
+ALTER SEQUENCE entries_entry_id_seq RESTART WITH 5;
+ALTER SEQUENCE subjects_subject_id_seq RESTART WITH 2;
+ALTER SEQUENCE value_types_value_type_id_seq RESTART WITH 2;
